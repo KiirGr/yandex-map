7633ca01168ee47ab61c77fb838d76fa0e5238b4
KiirGr <workmailkirill@gmail.com>
2024-03-09 19:43:35 +0300

    added prettier with config, lint-staged, eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,30 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
+  env: { 
+    browser: true, 
+    commonjs: true, 
+    es2020: true 
+  },
+  extends: [    
+    'standard',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "airbnb-base",
+    "prettier",
+  ],
+  overrides: [
+    {
+        env: {
+          browser: true,
+          node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+            sourceType: 'script',
+        },
+    },
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
