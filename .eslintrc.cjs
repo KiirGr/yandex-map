42c69b42f022ec3b03043e44a6b15e85d7b87a60
KiirGr <workmailkirill@gmail.com>
2024-03-09 21:38:15 +0300

    checking linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,43 @@
 module.exports = {
   root: true,
-  env: { 
-    browser: true, 
-    commonjs: true, 
-    es2020: true 
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
     "airbnb",
     "prettier",
   ],
   overrides: [
     {
-        env: {
-          browser: true,
-          node: true,
-        },
-        files: ['.eslintrc.{js,cjs}'],
-        parserOptions: {
-            sourceType: 'script',
-        },
+      env: {
+        browser: true,
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "import/extensions": [
       "error",
-       "ignorePackages",
-       {         
-         "jsx": "always",
-       }
+      "ignorePackages",
+      {
+        jsx: "always",
+      },
     ],
   },
-}
+};
