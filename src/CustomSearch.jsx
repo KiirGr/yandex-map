fda76444bedce979f29040e311bed947113c7c72
KiirGr <workmailkirill@gmail.com>
2024-03-21 19:28:50 +0300

    added func to send data from form to main component

--- src/CustomSearch.jsx
+++ src/CustomSearch.jsx
@@ -1,29 +1,31 @@
 import React, {useState, useRef, useEffect} from "react";
 
-function CustomSearch() {
-  const [adressForm, setAdressform] = React.useState('')
-  const [adressCoordinates, setAdresscoordinates] = React.useState('') 
+function CustomSearch({childToParent}) {
+  const [adressForm, setAdressform] = useState('')
+  const [adressCoordinates, setAdresscoordinates] = useState('')   
 
+  
   function handleSubmit(event) {
     event.preventDefault()
     setAdressform(event.target.suggest.value)
     
-    var myGeocoder = ymaps.geocode(event.target.suggest.value);
+    const myGeocoder = ymaps.geocode(event.target.suggest.value);
     myGeocoder.then(
         function (res) {
           setAdresscoordinates(res.geoObjects.get(0).geometry.getCoordinates());
+          console.log(adressCoordinates);
+          childToParent(res.geoObjects.get(0).geometry.getCoordinates());
         },
 
         function (err) {
           setAdresscoordinates('Не найдено');
         }
-    );
-  }  
+    );    
+  }
 
   return (
     <>      
       <p>Я засабмитил: {adressForm} </p>
-      <p>Координаты обьекта: {adressCoordinates} </p>
       <form onSubmit={handleSubmit}>
         <input type="text" id="suggest" />
         <button type="submit">Отправить</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useRef, useEffect} from "react";
 import ReactDOM from "react-dom/client";
-import MyMap from "./MyMap";
 import CustomMap from "./CustomMap";
 import CustomSearch from "./CustomSearch";
 import App from "./App";
@@ -13,23 +12,33 @@ ymaps.ready(init);
         zoom: 13
     });
     var suggestView1 = new ymaps.SuggestView('suggest');
-
-    // var myGeocoder = ymaps.geocode("Москва");
-
-    // myGeocoder.then(
-    //     function (res) {
-    //         alert('Координаты объекта :' + res.geoObjects.get(0).geometry.getCoordinates());
-    //     },
-
-    //     function (err) {
-    //         alert('Ошибка');
-    //     }
-    // );
   }
+
+  function Parent(){
+
+    const [adressCoordinates, setAdressCoordinates] = useState('')
+
+    const childToParent = (childdata) => {
+      setAdressCoordinates(childdata);
+    }
+
+    console.log(" долгота: "+adressCoordinates[0]+" широта: "+adressCoordinates[1]);
+
+    return (
+      <div className="App">
+       {adressCoordinates}
+        <div>
+          <CustomSearch childToParent={childToParent}/>
+        </div>
+      </div>
+    );
+  }  
+
+  export default Parent;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CustomSearch/>
-    <CustomMap />
+    <Parent/>
+    <CustomMap />    
   </React.StrictMode>,
 );
