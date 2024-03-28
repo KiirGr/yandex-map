a3fd6a4456c677c6df7c525e3745001b5c71004d
KiirGr <workmailkirill@gmail.com>
2024-03-28 16:38:30 +0300

    adding dots to map

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,83 @@
 import React, {useState, useRef, useEffect} from "react";
 import ReactDOM from "react-dom/client";
-import CustomMap from "./CustomMap";
+// import CustomMap from "./CustomMap";
 import CustomSearch from "./CustomSearch";
 import App from "./App";
 import "./index.css";
 
-  const init = () => {
-    const myMap = new ymaps.Map("map", {
+ let myMap;
+
+  let init = () => {
+    myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
         zoom: 13
     });
-    const suggestView1 = new ymaps.SuggestView('suggest');    
-     
+    const suggestView1 = new ymaps.SuggestView('suggest');   
+
   }
 
   ymaps.ready(init);
 
-  const Parent = (myMap) => {
+  const CustomMap = () => {
+
+    const width = 600
+    const height = 400
+    const dimensions = {
+      width: `${width}px`,
+      height: `${height}px`
+    }
+  
+    return (
+      <>      
+        <div id="map" style={dimensions}></div>
+      </>
+    );
+  }
+
+  const Parent = () => {
 
     const [adressCoordinates, setAdressCoordinates] = useState('')
 
     const childToParent = (childdata) => {
-      setAdressCoordinates(childdata);
-    }
+      setAdressCoordinates(childdata);      
+    }    
 
-    console.log(" долгота: "+adressCoordinates[0]+" широта: "+adressCoordinates[1]);    
+    if (typeof adressCoordinates[0] !== "undefined"){
+        // Создание геообъекта с типом точка (метка).
+        var myPlacemark = new ymaps.Placemark([adressCoordinates[0], adressCoordinates[1]]);        
+        
+        // Размещение геообъекта на карте.
+        myMap.geoObjects.add(myPlacemark);
+
+        let cordArr = [];
+
+        myMap.geoObjects.each(function (geoObject) {
+          if (geoObject instanceof ymaps.Placemark) {            
+            cordArr.push(geoObject.geometry.getCoordinates());
+          }
+        });
+
+        console.log(cordArr)
+
+        // const uniqueObjects = [...new Map(cordArr.map(item => [item.key, item])).values()];
+        // console.log(uniqueObjects); // [object1, object2, object3, ...]                   
+
+          ymaps.route([[adressCoordinates[0], adressCoordinates[1]]], {
+            multiRoute: false
+          }).done(function (route) {
+              route.options.set("mapStateAutoApply", true);
+              myMap.geoObjects.add(route);
+          }, function (err) {
+              throw err;
+          }, this);
+    }
 
     return (
       <div className="App">
-       {adressCoordinates}
+        {/* {adressCoordinates} */}
         <div>
           <CustomSearch childToParent={childToParent}/>
+          <CustomMap/>
         </div>
       </div>
     );
@@ -38,9 +85,6 @@ import "./index.css";
 
   export default Parent;
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(  
     <Parent/>
-    <CustomMap />    
-  </React.StrictMode>,
 );
