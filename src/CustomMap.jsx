9e41907205310811ff78c845e9c7c72a4a7bf814
KiirGr <workmailkirill@gmail.com>
2024-06-02 19:02:22 +0300

    completed todo tasks

--- src/CustomMap.jsx
+++ src/CustomMap.jsx
@@ -20,13 +20,15 @@ function CustomMap({ size, points = [] }) {
       return;
     }
 
-    // TODO: remove all points from the map
+    // TODO: remove all points from the map +++DONE+++
+    myMap.current.geoObjects.removeAll();
 
     if (points.length === 1) {
       const placemark = new window.ymaps.Placemark(points[0]);
 
-      myMap.current.geoObjects.add(placemark)
-      // TODO: center map here
+     myMap.current.geoObjects.add(placemark);
+      // TODO: center map here +++DONE+++
+      myMap.current.setCenter(points[0]);
       return;
     }
 
@@ -41,7 +43,12 @@ function CustomMap({ size, points = [] }) {
 
       alert(errorMsg);
     }, this);
-    // TODO: center map here
+    // TODO: center map here - +++ It works already +++
+  }
+
+  const clearMap = () => {
+    myMap.current.geoObjects.removeAll();
+    points.length=0;
   }
 
   useEffect(() => {
@@ -52,7 +59,9 @@ function CustomMap({ size, points = [] }) {
   }, [points])
 
   return (
-    <div id="map" style={dimensions}/>
+    <div id="map" style={dimensions}>
+      <button onClick={clearMap}>Очистить карту</button>
+    </div>    
   );
 }
 
